7e8af9fb658551790fa18ee06fc9cfc751d8eeb1
Carlo10K <carlosortegadiaz50@gmail.com>
2024-06-02 17:44:05 -0600

    widgets de home Screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner_sqlite/widgets/custom_navigator_bar.dart';
+import 'package:qr_scanner_sqlite/widgets/scan_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historial'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.delete_forever))
+        ],
+      ),
+      body: const Center(
         child: Text('HomeScreen'),
       ),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
